3a517a291147c839b6fe6ef316e4a0b84d35964c
Noumi Akira <akira.noumi@gmail.com>
2024-11-06 17:16:24 +0900

    Add PICO_BOARD=waveshare_rp2040_zero

--- mk.sh
+++ mk.sh
@@ -5,14 +5,18 @@ build() {
     mkdir $1
 
     cd $1
-    cmake -G Ninja $2 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+    if [ ${3:+t} ]; then
+        cmake -G Ninja $3 $2 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+    else
+        cmake -G Ninja $2 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+    fi
     ninja
     cd ..
 }
 
 build build-probe ''
 build build-xiao -DXIAO=1
-build build-zero -DZERO=1
+build build-zero -DZERO=1 -DPICO_BOARD=waveshare_rp2040_zero
 
 rm -rf uf2
 mkdir uf2
